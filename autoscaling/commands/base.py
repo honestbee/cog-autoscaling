4332b6d21023b7bd156dbb083529bb0f18422221
Charles Martinot <charles.martinot@honestbee.com>
2017-04-20 15:53:53 +0800

    Get vars from env

--- autoscaling/commands/base.py
+++ autoscaling/commands/base.py
@@ -3,9 +3,9 @@ from cog.command import Command
 class AutoscalingBase(Command):
   def __init__(self):
     super().__init__()
-    self.aws_access_key_id = None
-    self.aws_secret_access_key = None
-    self.aws_default_region = None
+    self.aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID', None)
+    self.aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', None)
+    self.aws_default_region = os.getenv('AWS_DEFAULT_REGION', None)
 
   def prepare(self):
     self.aws_access_key_id = self.config("aws_access_key_id")
@@ -18,4 +18,4 @@ class AutoscalingBase(Command):
 
     self.region = self.config("region")
     if self.region == None:
-      self.fail("Missing dynamic configuration variable 'region'.")
+      self.fail("Missing dynamic configuration variable 'aws_default_region'.")
