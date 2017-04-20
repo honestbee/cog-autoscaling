78031e90e1b8b282c453b4f003d414031bc8adfd
Charles Martinot <charles.martinot@honestbee.com>
2017-04-20 17:41:58 +0800

    Add permissions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
     name = "autoscaling",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "Cog commands for AWS autoscaling",
     author = "Charles Martinot",
     author_email = "charles.martinot@honestbee.com",
