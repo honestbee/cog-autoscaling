b3a95061f8e9f2e9aecc61c7ee232672db935f6d
Charles Martinot <charles.martinot@honestbee.com>
2017-04-21 14:53:03 +0800

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
     name = "autoscaling",
-    version = "0.1.11",
+    version = "0.2.0",
     description = "Cog commands for AWS autoscaling",
     author = "Charles Martinot",
     author_email = "charles.martinot@honestbee.com",
